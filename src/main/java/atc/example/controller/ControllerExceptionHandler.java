b59980e3daefb25c79b354dbca80764857854326
Darryl Anderson <6097929+darrylanderson@users.noreply.github.com>
2018-02-20 16:35:01 -0600

    Added chassis support for monitoring, tracking, security, logging, and service discovery/registration.

---
+++ src/main/java/atc/example/controller/ControllerExceptionHandler.java
@@ -0,0 +1,24 @@
+package atc.example.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class ControllerExceptionHandler
+{
+    private static final Logger logger = LoggerFactory.getLogger( ControllerExceptionHandler.class );
+
+    @ResponseStatus ( value = HttpStatus.INTERNAL_SERVER_ERROR )
+    @ExceptionHandler ( Exception.class )
+    @ResponseBody
+    public String handleInternalError ( final Exception e )
+    {
+        logger.error( e.getMessage(), e );
+        return HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase();
+    }
+}
